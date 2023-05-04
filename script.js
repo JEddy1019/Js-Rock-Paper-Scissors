0e1b8091514b7b100be3c4f905fc3a5a1a63442e
JEddy1019 <back.up.plan.1019@gmail.com>
2023-05-04 10:57:44 -0400

    Create a round function
    Made a function that simulates a round between player and computer

--- script.js
+++ script.js
@@ -9,20 +9,9 @@ function random(number) {
   
 
 function getComputerChoice(){
-    // let num = random(3);
-    // console.log(num);
-    // let choice;
-    // if (num=2){
-    //     return choice='rock';
-    // }else if (num=1){
-    //     return choice='paper'
-    // }else if (num=0){
-    //     return choice='scissors'
-    // }else{
-    //     return choice='error' YOU FORGOT SEMI COLEN!
-    // }
+    
     let num = random(3);
-    console.log(num);
+    // console.log(num);
     let choice;
     switch (num){
         case num = 2:
@@ -38,5 +27,57 @@ function getComputerChoice(){
             return choice = 'error';
     }
 }
-console.log(getComputerChoice());
-// console.log(random(3));
+// console.log(getComputerChoice());
+
+let player ='rock'; //testing variable
+// let CPU = 'paper';   //testing variable
+
+function round (CPU, player){
+    let result;
+    // CPU = CPU.toLowerCase();//testing variable
+    player = player.toLowerCase();
+
+    // switch (CPU, player){
+
+    //     case (CPU = 'rock') && (player ='rock'):
+    //     case (CPU = 'paper') && (player ='paper'):
+    //     case (CPU = 'scissors') && (player ='scissors'):
+    //         return result = 'Draw!';
+    //         break;
+
+    //     case (CPU = 'rock') && (player = 'paper'):
+    //     case (CPU = 'paper') && (player = 'scissors'):    
+    //     case (CPU = 'scissors') && (player = 'rock'):
+    //         return result = `You win! ${player} beats ${CPU}!`;
+    //         break;
+
+    //     case (CPU = 'rock') && (player = 'scissors'):
+    //     case (CPU = 'paper') && (player = 'rock'): 
+    //     case (CPU = 'scissors') && (player = 'paper'):
+    //         return result = `You lose! ${CPU} beats ${player}!`;
+    //         break; 
+
+    //     default:
+    //         return result = 'error';
+    // }
+
+
+    if (((CPU == 'rock') && (player =='rock')) || ((CPU == 'paper') && (player =='paper')) || ((CPU == 'scissors') && (player =='scissors')) ){
+
+        return result = 'Draw!';
+
+    }else if(((CPU =='rock') && (player == 'paper')) || ((CPU =='paper') && (player == 'scissors')) || ((CPU == 'scissors') && (player == 'rock'))){
+
+        return result = `You win! ${player} beats ${CPU}!`;
+
+    }else if(((CPU == 'rock') && (player == 'scissors')) || ((CPU == 'paper') && (player == 'rock')) ||  ((CPU == 'scissors') && (player == 'paper'))){
+
+        return result = `You lose! ${CPU} beats ${player}!`;
+
+    }else{
+        return result = 'error';
+    }
+
+}
+// console.log(round(CPU, player));
+console.log(round(getComputerChoice(), player));
