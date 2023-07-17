782c98453576d113b6fae844648a134faacc06bb
JEddy1019 <back.up.plan.1019@gmail.com>
2023-07-17 15:59:20 -0400

    Create Button Options for Player
    Created buttons for the player to choose there move
    Added css style sheet

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-// function generateRandomInteger(max) {
-//     return Math.floor(Math.random() * max) + 1;
-// }
-
 function random(number) {
     const result = Math.floor(Math.random() * number);
     return result;
   }
   
-
-function getComputerChoice(){
-    
+function getComputerChoice(){  
     let num = random(3);
-    // console.log(num);
     let choice;
     switch (num){
         case num = 2:
@@ -27,70 +20,53 @@ function getComputerChoice(){
             return choice = 'error';
     }
 }
-// console.log(getComputerChoice());
 
-let player ='rock'; //testing variable
+
+
+// let player ='rock'; //testing variable
 // let CPU = 'paper';   //testing variable
 
-function round (CPU, player){
+function round (player){
     let result;
-    // CPU = CPU.toLowerCase();//testing variable
-    player = player.toLowerCase();
-
-    // switch (CPU, player){
-
-    //     case (CPU = 'rock') && (player ='rock'):
-    //     case (CPU = 'paper') && (player ='paper'):
-    //     case (CPU = 'scissors') && (player ='scissors'):
-    //         return result = 'Draw!';
-    //         break;
-
-    //     case (CPU = 'rock') && (player = 'paper'):
-    //     case (CPU = 'paper') && (player = 'scissors'):    
-    //     case (CPU = 'scissors') && (player = 'rock'):
-    //         return result = `You win! ${player} beats ${CPU}!`;
-    //         break;
-
-    //     case (CPU = 'rock') && (player = 'scissors'):
-    //     case (CPU = 'paper') && (player = 'rock'): 
-    //     case (CPU = 'scissors') && (player = 'paper'):
-    //         return result = `You lose! ${CPU} beats ${player}!`;
-    //         break; 
-
-    //     default:
-    //         return result = 'error';
-    // }
-
+    let CPU = getComputerChoice();
+    // player = player.toLowerCase();
 
     if (((CPU == 'rock') && (player =='rock')) || ((CPU == 'paper') && (player =='paper')) || ((CPU == 'scissors') && (player =='scissors')) ){
-
         return result = 'Draw!';
-
     }else if(((CPU =='rock') && (player == 'paper')) || ((CPU =='paper') && (player == 'scissors')) || ((CPU == 'scissors') && (player == 'rock'))){
-
         return result = `You win! ${player} beats ${CPU}!`;
-
     }else if(((CPU == 'rock') && (player == 'scissors')) || ((CPU == 'paper') && (player == 'rock')) ||  ((CPU == 'scissors') && (player == 'paper'))){
-
         return result = `You lose! ${CPU} beats ${player}!`;
-
     }else{
         return result = 'error';
     }
 
 }
-// console.log(round(CPU, player));
-// console.log(round(getComputerChoice(), player));
 
 
+// function game (){
+//     for (let x = 0;x<5; x++){
+//         let player = prompt('Choose between rock, paper and scissors.');
+//         let count = x + 1;
+//         console.log(`Round ${count}!`);
+//         console.log(round(getComputerChoice(), player));
+//     }
+//     console.log('Game has finished!');
+// }
+// console.log(game());
 
-function game (){
-    for (let x = 0;x<5; x++){
-        let player = prompt('Choose between rock, paper and scissors.');
-        let count = x + 1;
-        console.log(`Round ${count}!`);
-        console.log(round(getComputerChoice(), player));
-    }
-    console.log('Game has finished!');
-}
-console.log(game());
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', e=>{
+    console.log(round('rock'))
+});
+paper.addEventListener('click', e=>{
+    console.log(round('paper'))
+});
+scissors.addEventListener('click', e=>{
+    console.log(round('scissors'))
+});
+
+
