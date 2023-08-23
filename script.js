93a2dbbf9aec808c44b774f9fddf74495b5d9f7f
JEddy1019 <back.up.plan.1019@gmail.com>
2023-08-23 11:08:36 -0400

    Added win counter and winner declaration
    Added win counter under rps buttons
    Added a win declaration that triggers at 5 wins from either player
    Added game description under main header

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const results = document.querySelector('.results');
+const runningScore = document.querySelector('.running-score');
+const div = document.createElement('div');
+const div2 = document.createElement('div');
+const div3 = document.createElement('div');
+const div4 = document.createElement('div');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const playerScore = document.querySelector('#player-score');
+const cpuScore = document.querySelector('#cpu-score');
+const winner = document.querySelector('.winner');
+let playerCount = 0;
+let cpuCount = 0;
+let winStatementCheck = 1; // if one win statement is already displayed, the other won't be
+let scoreCount = round();
+
 function random(number) {
     const result = Math.floor(Math.random() * number);
     return result;
@@ -24,52 +41,118 @@ function getComputerChoice(){
 function round (player){
     let result;
     let CPU = getComputerChoice();
-    // player = player.toLowerCase();
+    let score = 0;
 
     if (((CPU == 'rock') && (player =='rock')) || ((CPU == 'paper') && (player =='paper')) || ((CPU == 'scissors') && (player =='scissors')) ){
-        return result = 'Draw!';
+        score = 0;
+        return result = ['Draw!', score];
     }else if(((CPU =='rock') && (player == 'paper')) || ((CPU =='paper') && (player == 'scissors')) || ((CPU == 'scissors') && (player == 'rock'))){
-        return result = `You win! ${player} beats ${CPU}!`;
+        score = 1;
+        return result = [`You win! ${player} beats ${CPU}!`, score];
     }else if(((CPU == 'rock') && (player == 'scissors')) || ((CPU == 'paper') && (player == 'rock')) ||  ((CPU == 'scissors') && (player == 'paper'))){
-        return result = `You lose! ${CPU} beats ${player}!`;
+        score = 2;
+        return result = [`You lose! ${CPU} beats ${player}!`, score];
     }else{
-        return result = 'error';
+        return result = ['error', score];
     }
-
+    return score;
 }
 
-// function game (){
-//     for (let x = 0;x<5; x++){
-//         let player = prompt('Choose between rock, paper and scissors.');
-//         let count = x + 1;
-//         console.log(`Round ${count}!`);
-//         console.log(round(getComputerChoice(), player));
-//     }
-//     console.log('Game has finished!');
-// }
-// console.log(game());
-
-const results = document.querySelector('.results');
-const runningScore = document.querySelector('.running-score');
-const winner = document.createElement('div');
-const div = document.createElement('div');
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
-
-
 rock.addEventListener('click', e=>{
-    div.textContent = `${round('rock')}`;
+    let answer = round('rock');
+    div.textContent = `${answer[0]}`;
     results.append(div);
+
+    if (answer[1] === 1){
+        playerCount= playerCount+1;
+        div2.textContent = `${playerCount}`;
+        playerScore.append(div2);
+    }
+    if (answer[1] === 2){
+        cpuCount= cpuCount+1;
+        div3.textContent = `${cpuCount}`;
+        cpuScore.append(div3);
+    }
+
+    if (playerCount== 5 && winStatementCheck == 1){
+        div4.textContent = 'The Player has won the game! Refresh page for a new game';
+        winner.append(div4);
+        winStatementCheck = 0;
+    }
+
+    if (cpuCount == 5 && winStatementCheck == 1){
+        div4.textContent = 'The Computer has won the game! Refresh page for a new game';
+        winner.append(div4);
+        winStatementCheck = 0;
+    }
+
 });
+
 paper.addEventListener('click', e=>{
-    div.textContent = `${round('paper')}`;
+    let answer = round('paper');
+    div.textContent = `${answer[0]}`;
     results.append(div);
+
+    if (answer[1] === 1){
+        playerCount= playerCount+1;
+        div2.textContent = `${playerCount}`;
+        playerScore.append(div2);
+    }
+    if (answer[1] === 2){
+        cpuCount= cpuCount+1;
+        div3.textContent = `${cpuCount}`;
+        cpuScore.append(div3);
+    }
+
+    if (playerCount== 5 && winStatementCheck == 1){
+        div4.textContent = 'The Player has won the game! Refresh page for a new game';
+        winner.append(div4);
+        winStatementCheck = 0;
+    }
+
+    if (cpuCount == 5 && winStatementCheck == 1){
+        div4.textContent = 'The Computer has won the game! Refresh page for a new game';
+        winner.append(div4);
+        winStatementCheck = 0;
+    }
 });
+
 scissors.addEventListener('click', e=>{
-    div.textContent = `${round('scissors')}`;
+    let answer = round('scissors');
+    div.textContent = `${answer[0]}`;
     results.append(div);
+
+    if (answer[1] === 1){
+        playerCount= playerCount+1;
+        div2.textContent = `${playerCount}`;
+        playerScore.append(div2);
+    }
+    if (answer[1] === 2){
+        cpuCount= cpuCount+1;
+        div3.textContent = `${cpuCount}`;
+        cpuScore.append(div3);
+    }
+
+    if (playerCount== 5 && winStatementCheck == 1){
+        div4.textContent = 'The Player has won the game! Refresh page for a new game';
+        winner.append(div4);
+        winStatementCheck = 0;
+    }
+
+    if (cpuCount == 5 && winStatementCheck == 1){
+        div4.textContent = 'The Computer has won the game! Refresh page for a new game';
+        winner.append(div4);
+        winStatementCheck = 0;
+    }
 });
+
+
+
+
+
+
+
+
 
 
 
